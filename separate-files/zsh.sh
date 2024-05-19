538f9a56eaeba1f5a6d554f0f5549d05c3743bb9
Arima_key <127151745+ArimaKey@users.noreply.github.com>
2024-05-18 22:17:39 -0500

    feat: ohmyzsh added

--- separate-files/zsh.sh
+++ separate-files/zsh.sh
@@ -5,13 +5,15 @@ sudo ./separate-files/installer.sh "zsh"
 
 echo "[⚙️] Cambiando la terminal por defecto"
 # Configuramos ZSH como SHELL por defecto
-# sudo chsh -s $(which zsh) > /dev/null 2>&1
-# chsh -s $(which zsh) > /dev/null 2>&1
+sudo chsh -s $(which zsh) > /dev/null 2>&1
+chsh -s $(which zsh) > /dev/null 2>&1
+
+exec zsh
 
 # Instalamos ohmyZSH!
 echo "[-] Instalando OhMyZSH!"
-# sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
-# sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
+sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
 echo "[✓] OhMyZSH! se instaló correctamente"
 
 
