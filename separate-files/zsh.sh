b5b7fc896f949339670ac5fcc25255f46c66aab3
arima <arima@localhost.localdomain>
2024-05-19 21:13:30 -0400

    fix: Program unresponsive due to ZSH installation

--- separate-files/zsh.sh
+++ separate-files/zsh.sh
@@ -3,19 +3,21 @@
 # Instalamos ZSH
 sudo ./separate-files/installer.sh "zsh"
 
+touch $HOME/.zshrc
+sudo touch $HOME/.zshrc
+
 echo "[⚙️] Cambiando la terminal por defecto"
 # Configuramos ZSH como SHELL por defecto
 sudo chsh -s $(which zsh) > /dev/null 2>&1
 chsh -s $(which zsh) > /dev/null 2>&1
 
-touch $HOME/.zshrc
-sudo touch $HOME/.zshrc
+
 
 # Instalamos ohmyZSH!
-echo "[-] Instalando OhMyZSH!"
-zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
-sudo zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
-echo "[✓] OhMyZSH! se instaló correctamente"
+# echo "[-] Instalando OhMyZSH!"
+# zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
+# sudo zsh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" > /dev/null 2>&1
+# echo "[✓] OhMyZSH! se instaló correctamente"
 
 
 function ctrl_c() {
